230f3ca8a5e17acd411f6afd12eb42dc1374974d
wuhailong_ext <wuhailong_ext@cop.cmsoft.com.cn>
2022-07-14 16:12:13 +0800

    hot-fix commit

--- cloud-provider-hystrix-payment8001/src/main/java/com/hailong/study/HystrixPaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/hailong/study/HystrixPaymentController.java
@@ -32,9 +32,6 @@ public class HystrixPaymentController {
   public String getTimeOut(@PathVariable("id") Integer id) {
     String result = service.PaymentInfo_TimeOut(id);
     log.info("result:" + result);
-    System.out.println("测试分支");
-    System.out.println("master测试");
-    System.out.println("hot-fix测试");
     return result;
   }
 
