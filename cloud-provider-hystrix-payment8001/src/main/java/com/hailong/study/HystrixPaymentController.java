290c2c8447f32c164eb306e26bb4533a844a8d18
wuhailong_ext <wuhailong_ext@cop.cmsoft.com.cn>
2022-07-14 13:55:36 +0800

    hot-fix first commit

--- cloud-provider-hystrix-payment8001/src/main/java/com/hailong/study/HystrixPaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/hailong/study/HystrixPaymentController.java
@@ -17,20 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class HystrixPaymentController {
+
   @Autowired
   HystrixPaymentService service;
 
   @GetMapping("/payment/hystrix/ok/{id}")
-  public String getOk(@PathVariable("id") Integer id){
-     String result = service.PaymentInfo_Ok(id);
-     log.info("result:"+result);
-     return result;
+  public String getOk(@PathVariable("id") Integer id) {
+    String result = service.PaymentInfo_Ok(id);
+    log.info("result:" + result);
+    return result;
   }
 
   @GetMapping("/payment/hystrix/timeOut/{id}")
-  public String getTimeOut(@PathVariable("id")Integer id){
-    String result =  service.PaymentInfo_TimeOut(id);
-    log.info("result:"+result);
+  public String getTimeOut(@PathVariable("id") Integer id) {
+    String result = service.PaymentInfo_TimeOut(id);
+    log.info("result:" + result);
+    System.out.println("测试分支");
     return result;
   }
 
